d34dbe7968a6571bedab092cf8e63e9131a00263
stassie-lzrv <adlazareva@edu.hse.ru>
2023-07-01 04:51:11 +0300

    -

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["FileCachePackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FileCachePackage",
-            dependencies: []),
+            dependencies: [ .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack")]),
         .testTarget(
             name: "FileCachePackageTests",
             dependencies: ["FileCachePackage"]),
---
+++ Sources/FileCachePackage/FileCache.swift
@@ -0,0 +1,67 @@
+//
+//  FileCache.swift
+//  
+//
+//  Created by Настя Лазарева on 01.07.2023.
+//
+
+import Foundation
+import CocoaLumberjackSwift
+
+public protocol JSONConvertible {
+    associatedtype Item: JSONConvertible
+    init?(json: [String: Any])
+    var json: [String: Any] { get }
+    static func parse(json: Any) -> Item?
+}
+
+
+public protocol IdentifiableType {
+    var id: String { get }
+}
+
+public class FileCache<Item: JSONConvertible & IdentifiableType> {
+    let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+     var todoItemCollection : [Item] = []
+    
+    func addNewTask(_ newTask : Item){
+        if let ind = todoItemCollection.firstIndex(where: {$0.id == newTask.id}){
+            todoItemCollection[ind] = newTask
+        } else {
+            todoItemCollection.append(newTask)
+        }
+    }
+    
+    func deleteTask(with id: String){
+        todoItemCollection.removeAll(where: {$0.id == id})
+    }
+    
+    func saveJSON(filename: String){
+        let urlPath = url.appendingPathComponent(filename)
+        let jsonItems = todoItemCollection.map({$0.json})
+        do {
+            let data = try JSONSerialization.data(withJSONObject: jsonItems, options: .prettyPrinted)
+            try data.write(to: urlPath)
+        } catch {
+            DDLogError("Error saving data")
+        }
+    }
+    
+    func loadJSON(filename: String){
+        let urlPath = url.appendingPathComponent(filename)
+        if FileManager.default.fileExists(atPath: urlPath.path),
+           let data = try? Data(contentsOf: urlPath),
+           let jsonItems = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+            todoItemCollection = fetchItemsFromJson(jsonItems) ?? []
+        } else {
+            DDLogError("File not found")
+        }
+    }
+    
+    func fetchItemsFromJson(_ json: [Any]) -> [Item]? {
+           return json.compactMap { Item.parse(json: $0) as? Item }
+       }
+    
+    
+}
